2bc35e02ff308f3fc0dec3e22c0e36088496472d
Kadir <kadirhana91@gmail.com>
2025-11-10 18:22:43 +0200

    logger

--- app/Helpers/Logger.php
+++ app/Helpers/Logger.php
@@ -2,13 +2,37 @@
 namespace App\Helpers;
 class Logger {
     private static $logFile = __DIR__ . '/../../storage/logs/app.log';
+    
     private static function ensurePath(){
         $dir = dirname(self::$logFile);
         if (!is_dir($dir)) @mkdir($dir, 0777, true);
     }
+    
     public static function log($message) {
         self::ensurePath();
         $date = date('Y-m-d H:i:s');
         file_put_contents(self::$logFile, "[$date] $message\n", FILE_APPEND);
+    }
+    
+    public static function info($message) {
+        self::logWithLevel('INFO', $message);
+    }
+    
+    public static function error($message) {
+        self::logWithLevel('ERROR', $message);
+    }
+    
+    public static function warning($message) {
+        self::logWithLevel('WARNING', $message);
+    }
+    
+    public static function debug($message) {
+        self::logWithLevel('DEBUG', $message);
+    }
+    
+    private static function logWithLevel($level, $message) {
+        self::ensurePath();
+        $date = date('Y-m-d H:i:s');
+        file_put_contents(self::$logFile, "[$date] [$level] $message\n", FILE_APPEND);
     }
 }
